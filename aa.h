3d986b0cfed6bea9290f507b0252589ea484ece3
rta <k@l.fr>
2015-01-30 17:00:18 +0100

    encours

--- aa.h
+++ aa.h
@@ -38,7 +38,7 @@ typedef enum {AA_FT_COURIER, AA_FT_LUCIDA, AA_FT_CONSOLA} AaFontId;
 typedef enum {AA_PAL_NONE, AA_PAL_MONOCHROME, AA_PAL_ANSI_16, AA_PAL_FREE_MONO, AA_PAL_FREE_16, AA_PAL_FREE_64, AA_PAL_FREE_256, AA_PAL_MAX} AaPaletteId;
 typedef enum {AA_ALG_PIXEL_11, AA_ALG_VECTOR_DST, AA_ALG_VECTOR_DST_FILL, AA_ALG_VECTOR_11, AA_ALG_VECTOR_11_FILL, AA_ALG_MAX} AaAlgorithmId;
 
-
+#pragma pack(1)
 typedef struct {
     BYTE rgbRed;
     BYTE rgbGreen;
@@ -69,11 +69,11 @@ typedef struct {
     BYTE* colors;
     AaPalette* palette;
 } AaImage;
-
+#pragma pack()
 
 
 MYEXPORT bool aa_init_font_default(AaFontId id, const char* subset, AaFont* res);
-MYEXPORT bool aa_init_font_from_picture(const char* font_picture_path, const char* font_carmap, const char* subset, AaFont* res);
+bool aa_init_font_from_picture(const char* font_picture_path, const char* font_carmap, const char* subset, AaFont* res);
 
 MYEXPORT bool aa_convert(FIBITMAP* image, AaAlgorithmId algorithm, AaFont* font, AaImage* res, int lines = DEFAULT_LINES, int working_height = DEFAULT_WORKING_HEIGHT, int translation = DEFAULT_TRANSLATION, float penalty = DEFAULT_PENALTY, AaPaletteId palette_id = DEFAULT_PALETTEID, float sigma = DEFAULT_SIGMA, int canny_min = DEFAULT_CANNY_HYS_MIN, int canny_max = DEFAULT_CANNY_HYS_MAX, float meanshift_r2 = DEFAULT_MEANSHIFT_R2, float meanshift_d2 = DEFAULT_MEANSHIFT_D2, int meanshift_n = DEFAULT_MEANSHIFT_N, int meanshift_iterations = DEFAULT_MEANSHIFT_ITERATIONS);
 
